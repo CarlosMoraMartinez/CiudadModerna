0613a06472c60d4fda6104f273dadd808c4380dc
camoma <carmoma9@gmail.com>
2024-04-27 12:14:51 +0200

    Changed filename

--- run_swipl.sh
+++ run_swipl.sh
@@ -1,3 +1,3 @@
-swipl -s mora_martinez_inmobiliarias.pl -g run_todo. -t halt.
+swipl -s ciudad_moderna.pl -g run_todo. -t halt.
 
-swipl -s mora_martinez_inmobiliarias.pl -g run_lista_visitar. -t halt
+swipl -s ciudad_moderna.pl -g run_lista_visitar. -t halt
